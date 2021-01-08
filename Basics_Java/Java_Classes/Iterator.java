05a0e22ac492f233b7a2099cfcc2fd13d2e92ed4
KristoferMar <kristofer@brostu.dk>
2021-01-08 13:35:51 +0100

    Created technical example of iterator

---
+++ Basics_Java/Java_Classes/Iterator.java
@@ -0,0 +1,50 @@
+public class main {
+    public static void main(String[] args) {
+
+        // Using the iterator()
+
+        // Make a collection
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+
+        // Get the iterator
+        Iterator<String> it = cars.iterator();
+
+        // Print the first item
+        System.out.println(it.next());
+    }
+
+    public static LoopIterator() {
+        // Iterator methods used for looping
+
+        //hasNext() and next()
+
+        while(it.hasNext()) {
+            System.out.println(it.next());
+        }
+    }
+
+    public static RemoveIterator() {
+        // Iterator mothod used to remove items.
+
+        // remove()
+
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+            numbers.add(12);
+            numbers.add(8);
+            numbers.add(2);
+            numbers.add(23);
+            Iterator<Integer> it = numbers.iterator();
+            while(it.hasNext()) {
+                Integer i = it.next();
+
+                if(i < 10) {
+                    it.remove();
+                }
+            }
+            System.out.println(numbers);
+    }
+}
